5c66a8db2c99a98a4b032446892f541c107d4fa0
xiaoyur347 <xiaoyur347@gmail.com>
2017-05-24 09:48:18 +0800

    add lambda test

---
+++ c++11/lambda_test.cpp
@@ -0,0 +1,62 @@
+#include <gtest/gtest.h>
+
+TEST(lambda, simple)
+{
+	int girls = 3;
+	int boys = 4;
+	auto totalChild = [](int x, int y) -> int {return x + y;};
+	ASSERT_EQ(totalChild(girls, boys), 7);
+}
+
+TEST(lambda, capture)
+{
+	int girls = 3;
+	int boys = 4;
+	auto totalChild = [girls, boys]() -> int {return girls + boys;};
+	ASSERT_EQ(totalChild(), 7);
+}
+
+TEST(lambda, capture_value)
+{
+	int girls = 3;
+	int boys = 4;
+	auto totalChild = [=]() -> int {return girls + boys;};
+	ASSERT_EQ(totalChild(), 7);
+}
+
+TEST(lambda, capture_reference)
+{
+	int girls = 3;
+	int boys = 4;
+	auto totalChild = [&]() -> int {return girls + boys;};
+	ASSERT_EQ(totalChild(), 7);
+}
+
+class CaptureClass
+{
+public:
+	CaptureClass(int girls, int boys)
+		:girls_(girls),
+		 boys_(boys)
+	{
+
+	}
+	int capture_this()
+	{
+		auto totalChild = [=]() -> int {return girls_ + boys_;};
+		return totalChild();
+	}
+	~CaptureClass()
+	{
+
+	}
+private:
+	int girls_;
+	int boys_;
+};
+
+TEST(lambda, capture_this)
+{
+	CaptureClass c(3, 4);
+	ASSERT_EQ(c.capture_this(), 7);
+}
